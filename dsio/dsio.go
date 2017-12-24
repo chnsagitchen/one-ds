2158a1c39f829a6eef99099a7e5069bfabc51e26
ChenWei <chenwei@ChenWeis-MacBook-Pro.local>
2017-12-24 11:46:19 +0800

    add tcp transport

--- dsio/dsio.go
+++ dsio/dsio.go
@@ -6,6 +6,9 @@ import (
 	"os"
 	"bufio"
 	"bytes"
+	"strings"
+	"strconv"
+	"fmt"
 )
 
 // A WAL record to persist in the fs
@@ -18,24 +21,31 @@ type DSWALRecordWriter struct {
 	CurSegWALOffset int64
 	CurSegRecordOffset int64
 	FH *os.File
+	RFH *os.File
+	MemTable map[string]string
 }
 
 // Return a new DSWALRecordWriter instance
 func New() *DSWALRecordWriter  {
 	fh, err := os.Create("dat")
-	if err != nil {
+	rfh, rErr := os.Open("dat")
+	if err != nil || rErr != nil {
 		panic(err)
+		panic(rErr)
 	}
 
 	return &DSWALRecordWriter{
 		CurSegWALOffset: 0,
 		CurSegRecordOffset: 0,
 		FH: fh,
+		RFH: rfh,
+		MemTable: map[string]string{},
 	}
 }
 
 // The method is to persist the WAL in the file system
 func (dsw *DSWALRecordWriter) WriteRecord(dsLogRecord *DSLogRecord) (int64, error) {
+	fmt.Println(dsw.FH == nil)
 	bufFileHandler := bufio.NewWriter(dsw.FH)
 
 	buffer := bytes.NewBufferString(dsLogRecord.RecordKey)
@@ -54,9 +64,36 @@ func (dsw *DSWALRecordWriter) WriteRecord(dsLogRecord *DSLogRecord) (int64, erro
 
 	bufFileHandler.Flush()
 
+	buffer = bytes.NewBufferString(strconv.FormatInt(dsw.CurSegRecordOffset,10))
+	buffer.WriteString("#")
+	buffer.WriteString(strconv.FormatInt(int64(len(dsLogRecord.RecordKey)),10))
+
+	dsw.MemTable[dsLogRecord.RecordKey] = buffer.String()
+
+	fmt.Printf("MemTable: %v", dsw.MemTable)
+
 	return dsw.CurSegRecordOffset, nil
+}
+
+// This method is to read the stored record from the file system
+func (dsw *DSWALRecordWriter) ReadRecord(dsLogRecord *DSLogRecord) (string, error) {
+	dataRefInfo := strings.Split(dsw.MemTable[dsLogRecord.RecordKey], "#")
+	dataStartIdx, _ := strconv.ParseInt(dataRefInfo[0], 10, 64)
+	dataLen, _ := strconv.Atoi(dataRefInfo[1])
+	_, err := dsw.RFH.Seek(dataStartIdx, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	bts := make([]byte, dataLen)
+	dsw.RFH.Read(bts)
+	s := string(bts)
+	fmt.Printf("Query %s at position %d is %s", dsLogRecord.RecordKey, dataStartIdx, s)
+
+	return s, nil
 }
 
 func (dsw *DSWALRecordWriter) Stop() {
 	dsw.FH.Close()
+	dsw.RFH.Close()
 }
--- main.go
+++ main.go
@@ -3,15 +3,61 @@ package main
 import (
 	"github.com/chnsagitchen/one-ds/dsio"
 	"fmt"
+	"net"
+	"bufio"
+	"strings"
 )
 
-func main() {
-	record := dsio.DSLogRecord{
-		"hello",
-		"world",
+func handleDBCommand(command string, dsw *dsio.DSWALRecordWriter) string {
+	commandArr := strings.Split(command, "|")
+	record := &dsio.DSLogRecord {
+		RecordKey: commandArr[1],
 	}
+
+	result := "ACK"
+	if "GET" == commandArr[0] {
+		result, _ = dsw.ReadRecord(record)
+		fmt.Printf("result: %d", result)
+	} else if "PUT" == commandArr[0] {
+		record.RecordVal = commandArr[2]
+		offset, _ := dsw.WriteRecord(record)
+		fmt.Printf("offset: %d", offset)
+	}
+
+	return result
+}
+
+func handleDBConn(conn net.Conn, dsw *dsio.DSWALRecordWriter) {
+	defer conn.Close()
+
+	for {
+		// will listen for message to process ending in newline (\n)
+		command, _ := bufio.NewReader(conn).ReadString('#')
+		// output message received
+		fmt.Print("Command Received:", command)
+
+		result := handleDBCommand(command[:len(command) - 1], dsw)
+		conn.Write([]byte(result + "\n"))
+	}
+}
+
+func main() {
+	l, err := net.Listen("tcp", ":3130")
+	if err != nil {
+		fmt.Println("listen error:", err)
+		return
+	}
+
 	dsw := dsio.New()
-	offset, _ := dsw.WriteRecord(&record)
-	fmt.Printf("offset: %d", offset)
-	dsw.Stop()
+
+	for {
+		c, err := l.Accept()
+		if err != nil {
+			fmt.Println("accept error:", err)
+			break
+		}
+		// start a new goroutine to handle
+		// the new connection.
+		go handleDBConn(c, dsw)
+	}
 }
